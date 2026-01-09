8970cce2172b53bd8c8d7a9f15bf4c2c3a5d1ee9
yezinnie14 <yezinnie14@gmail.com>
2026-01-09 16:18:15 +0900

    update

--- src/pages/section/detail/Detailbar.js
+++ src/pages/section/detail/Detailbar.js
@@ -1,21 +1,15 @@
 import "./ReservationInfo.scss";
 import { useParking } from "../../../contexts/ParkingContext";
 
+const Detailbar = () => {
+  const { lotDetail } = useParking();
 
-const Detailbar = ( ) => {
-  const{ lotId}= useParking();
   return (
-<div className="parking-card">
-          <div className="parking-card-img" />
-          <div className="parking-card-texts">
-            <p className="parking-card-name">
-              {lotId?.parking_name || "주차장 이름"}
-            </p>
-            <p className="parking-card-addr">
-              {lotId?.address || "주차장 주소"} 
-            </p>
-          </div>
-        </div>  )
-}
+    <div className="parking-card">
+      <p>{lotDetail?.parking_name || "주차장 이름"}</p>
+      <p>{lotDetail?.address || "주차장 주소"}</p>
+    </div>
+  );
+};
 
-export default Detailbar
+export default Detailbar;
